8a7a1a2f35b5460f1c8962f3e4dfc0259fafe7d9
samuca23 <samuelchiodini@hotmail.com>
2023-01-17 19:45:53 -0300

    documentando

--- projeto_1.sql
+++ projeto_1.sql
@@ -1,3 +1,9 @@
+/* Arquivo para adicionar os comandos apresentados no Ebook
+ * @author Samuel Chiodini.
+ * @since 28/12/2022.
+ */
+ 
+-- CRIAÇÃO DAS TABELAS --
 CREATE TABLE mesas (
     id               int not null primary key,
     mesa_codigo      varchar(20),
@@ -16,6 +22,16 @@ CREATE TABLE funcionarios(
     data_atualizacao      timestamp
 );
 
+CREATE TABLE produtos (
+    id               int not null primary key,
+    produto_codigo   varchar(20),
+    produto_nome     varchar(60),
+    produto_valor    real,
+    produto_situacao varchar (1) default 'A',
+    data_criacao     timestamp,
+    data_atualizacao timestamp
+);
+
 CREATE TABLE vendas(
     id               int not null primary key,
     funcionario_id     int references funcionarios (id),
@@ -24,16 +40,6 @@ CREATE TABLE vendas(
     venda_valor      real,
     venda_total      real,
     venda_desconto   real,
-    data_criacao     timestamp,
-    data_atualizacao timestamp
-);
-
-CREATE TABLE produtos (
-    id               int not null primary key,
-    produto_codigo   varchar(20),
-    produto_nome     varchar(60),
-    produto_valor    real,
-    produto_situacao varchar (1) default 'A',
     data_criacao     timestamp,
     data_atualizacao timestamp
 );
@@ -60,6 +66,220 @@ CREATE TABLE comissoes(
     data_criacao timestamp,
     data_atualizacao timestamp
 )
+--FIM CRIAÇÃO TABELAS--
+
+
+
+
+-- ADICIONANDO PK E FK NAS TABELAS --
+/* Adicionando a PK na tabela */
+ALTER TABLE comissoes 
+ADD CONSTRAINT comissoes_pkey PRIMARY KEY(id);
+
+/* Adicionando a FK na tabela */
+ALTER TABLE comissoes 
+ADD FOREIGN KEY (funcionario_id) REFERENCES funcionarios(id);
+
+/* Verificando se o valor de venda total é positivo */
+ALTER TABLE vendas ADD CHECK (venda_total > 0)
+
+/* Verificando se o nome não é null*/
+ALTER TABLE funcionarios ADD CHECK (funcionario_nome <> null)
+--FIM--
+
+
+
+
+-- Criando SEQUENCES para as tabelas
+CREATE SEQUENCE mesa_id_seq;
+CREATE SEQUENCE vendas_id_seq;
+CREATE SEQUENCE itens_vendas_id_seq;
+CREATE SEQUENCE produtos_id_seq;
+CREATE SEQUENCE funcionario_id_seq;
+CREATE SEQUENCE comissoes_id_seq;
+--FRIM--
+
+
+
+
+-- Vinculando as SEQUENCES para suas respectivas tabelas
+ALTER TABLE mesas
+ALTER COLUMN id SET DEFAULT nextval('mesa_id_seq');
+
+ALTER TABLE vendas
+ALTER COLUMN id SET DEFAULT nextval('vendas_id_seq');
+
+ALTER TABLE ITENS_VENDAS
+ALTER COLUMN id SET DEFAULT nextval('itens_vendas_id_seq');
+
+ALTER TABLE produtos
+ALTER COLUMN id SET DEFAULT nextval('produtos_id_seq');
+
+ALTER TABLE funcionarios
+ALTER COLUMN id SET DEFAULT nextval('funcionario_id_seq');
+
+ALTER TABLE comissoes
+ALTER COLUMN id SET DEFAULT nextval('comissoes_id_seq');
+/* Para deletar uma SEQUENCE */
+/* Utilizar o CASCADE caso ela faça relação com outra tabela */
+DROP SEQUENCE funcionario_id_seq CASCADE;
+--FIM--
+
+
+
+
+-- Adicionando nova coluna na tabela
+ALTER TABLE comissoes
+ADD COLUMN data_pagamento timestamp;
+
+ALTER TABLE comissoes
+DROP COLUMN data_pagamento;
+--FIM--
+
+
+
+
+-- Inserindo novos dados as tabelas
+INSERT INTO mesas (mesa_codigo,
+                   mesa_situacao,
+                   data_criacao,
+                   data_atualizacao)
+    VALUES ('00010',
+            'A',
+            NOW(),
+            NOW());
+
+INSERT INTO funcionarios (funcionario_codigo,
+                          funcionario_nome,
+                          funcionario_situacao,
+                          funcionario_comissao,
+                          funcionario_cargo,
+                          data_criacao,
+                          data_atualizacao)
+    VALUES ('00007',
+            'Ana Paula Schimitt',
+            'A',
+            5,
+            'QUALIDADE',
+            NOW(),
+            NOW());
+            
+INSERT INTO produtos (produto_codigo,
+                      produto_nome,
+                      produto_valor,
+                      produto_situacao,
+                      data_criacao,
+                      data_atualizacao)
+    VALUES ('00006',
+            'Web Cam',
+            180,
+            'A',
+            NOW(),
+            NOW());
+            
+INSERT INTO vendas (funcionario_id,
+                    mesa_id,
+                    venda_codigo,
+                    venda_valor,
+                    venda_total,
+                    venda_desconto,
+                    venda_situacao,
+                    data_criacao,
+                    data_atualizacao)
+   VALUES (4,
+           4,
+           '00003',
+           2000,
+           2000,
+           0,
+          'A',
+          NOW(),
+          NOW());
+          
+          
+         
+INSERT INTO itens_vendas (produto_id,
+                          vendas_id,
+                          item_valor,
+                          item_quantidade,
+                          item_total,
+                          data_criacao,
+                          data_atualizacao)
+    VALUES (3,
+            3,
+            7,
+            3,
+            21,
+            NOW(),
+            NOW());
+--FIM--
+       
+       
+       
+            
+-- Criando FUNCTION para retornar a situação do funcionario
+CREATE OR REPLACE FUNCTION retorna_nome_funcionario(fund_id int)
+RETURNS text AS
+$$
+DECLARE
+nome text;
+situacao text;
+BEGIN
+    SELECT funcionario_nome,
+           funcionario_situacao
+      INTO nome, situacao
+      FROM funcionarios
+     WHERE id = fund_id;
+     
+     IF situacao = 'A' THEN
+        RETURN nome || ' Usuário Ativo';
+     ELSEIF situacao = 'I' THEN
+        RETURN nome || ' Usuário Inativo';
+     ELSEIF situacao IS NULL THEN
+        RETURN nome || ' Usuário Sem status';
+     ELSE
+        RETURN 'Usuário com status diferente de A e I';
+     END IF;
+END
+$$
+LANGUAGE plpgsql;
+
+DROP FUNCTION retorna_nome_funcionario;
+
+SELECT retorna_nome_funcionario(1);
+
+
+/* FUNCTION para calcular a porcentagem da comissao */
+CREATE OR REPLACE FUNCTION rt_valor_comissao(func_id int)
+RETURNS real AS
+$$
+DECLARE
+valor_comissao real;
+BEGIN
+    SELECT  funcionario_comissao
+       INTO valor_comissao
+       FROM funcionarios
+      WHERE id = func_id;
+      RETURN valor_comissao;
+END
+$$
+LANGUAGE plpgsql;
+          
+DROP FUNCTION rt_valor_comissao;
+          
+SELECT rt_valor_comissao(1);
+
+CREATE TABLE itens_vendas (
+    id               int not null primary key,
+    produto_id       int not null references produtos(id),
+    vendas_id        int not null references vendas(id),
+    item_valor       real,
+    item_quantidade  int,
+    item_total       real,
+    data_criacao     timestamp,
+    data_atualizacao timestamp
+);
+
 
 /* Adicionando a PK na tabela */
 ALTER TABLE comissoes 
@@ -434,7 +654,7 @@ INSERT INTO itens_vendas (produto_id,
             NOW());
             
             
-/* Criando FUNCTION para retornar a situação do funcionario */
+-- Criando FUNCTION para retornar a situação do funcionario
 CREATE OR REPLACE FUNCTION retorna_nome_funcionario(fund_id int)
 RETURNS text AS
 $$
@@ -460,13 +680,17 @@ BEGIN
 END
 $$
 LANGUAGE plpgsql;
+--FIM--
+
+
+
 
 DROP FUNCTION retorna_nome_funcionario;
 
 SELECT retorna_nome_funcionario(1);
 
 
-/* FUNCTION para calcular a porcentagem da comissao */
+--FUNCTION para calcular a porcentagem da comissao
 CREATE OR REPLACE FUNCTION rt_valor_comissao(func_id int)
 RETURNS real AS
 $$
@@ -481,256 +705,16 @@ BEGIN
 END
 $$
 LANGUAGE plpgsql;
-          
-DROP FUNCTION rt_valor_comissao;
-          
-SELECT rt_valor_comissao(1);
-         
-CREATE TABLE mesas (
-    id               int not null primary key,
-    mesa_codigo      varchar(20),
-    mesa_situacao    varchar (1) default 'A',
-    data_criacao     timestamp,
-    data_atualizacao timestamp
-);
-
-CREATE TABLE funcionarios(
-    id                    int not null primary key,
-    funcionario_codigo    varchar(20),
-    funcionario_nome      varchar(100),
-    funcionario_situacaio varchar(1) default 'A',
-    funcionario_comissao  real,
-    data_criacao          timestamp,
-    data_atualizacao      timestamp
-);
-
-CREATE TABLE vendas(
-    id               int not null primary key,
-    funcionario_id     int references funcionarios (id),
-    mesa_id          int references mesas (id),
-    venda_codigo     varchar(20),
-    venda_valor      real,
-    venda_total      real,
-    venda_desconto   real,
-    data_criacao     timestamp,
-    data_atualizacao timestamp
-);
-
-CREATE TABLE produtos (
-    id               int not null primary key,
-    produto_codigo   varchar(20),
-    produto_nome     varchar(60),
-    produto_valor    real,
-    produto_situacao varchar (1) default 'A',
-    data_criacao     timestamp,
-    data_atualizacao timestamp
-);
-
-CREATE TABLE itens_vendas (
-    id               int not null primary key,
-    produto_id       int not null references produtos(id),
-    vendas_id        int not null references vendas(id),
-    item_valor       real,
-    item_quantidade  int,
-    item_total       real,
-    data_criacao     timestamp,
-    data_atualizacao timestamp
-);
+--FIM--
 
 
-DROP TABLE comissoes;
 
-CREATE TABLE comissoes(
-    id int not null,
-    funcionario_id int,
-    comissao_valor real,
-    comissao_situacao varchar(1) default 'A',
-    data_criacao timestamp,
-    data_atualizacao timestamp
-)
-
-/* Adicionando a PK na tabela */
-ALTER TABLE comissoes 
-ADD CONSTRAINT comissoes_pkey PRIMARY KEY(id);
-
-/* Adicionando a FK na tabela */
-ALTER TABLE comissoes 
-ADD FOREIGN KEY (funcionario_id) REFERENCES funcionarios(id);
-
-/* Verificando se o valor de venda total é positivo */
-ALTER TABLE vendas ADD CHECK (venda_total > 0)
-
-/* Verificando se o nome não é null*/
-ALTER TABLE funcionarios ADD CHECK (funcionario_nome <> null)
-
-/* Criando SEQUENCES para as tabelas */
-CREATE SEQUENCE mesa_id_seq;
-CREATE SEQUENCE vendas_id_seq;
-CREATE SEQUENCE itens_vendas_id_seq;
-CREATE SEQUENCE produtos_id_seq;
-CREATE SEQUENCE funcionario_id_seq;
-CREATE SEQUENCE comissoes_id_seq;
-
-/* Vinculando as SEQUENCES para suas respectivas tabelas */
-ALTER TABLE mesas
-ALTER COLUMN id SET DEFAULT nextval('mesa_id_seq');
-
-ALTER TABLE vendas
-ALTER COLUMN id SET DEFAULT nextval('vendas_id_seq');
-
-ALTER TABLE ITENS_VENDAS
-ALTER COLUMN id SET DEFAULT nextval('itens_vendas_id_seq');
-
-ALTER TABLE produtos
-ALTER COLUMN id SET DEFAULT nextval('produtos_id_seq');
-
-ALTER TABLE funcionarios
-ALTER COLUMN id SET DEFAULT nextval('funcionario_id_seq');
-
-ALTER TABLE comissoes
-ALTER COLUMN id SET DEFAULT nextval('comissoes_id_seq');
-
-/* Para deletar uma SEQUENCE */
-/* Utilizar o CASCADE caso ela faça relação com outra tabela */
-DROP SEQUENCE funcionario_id_seq CASCADE;
-
-
-/* Adicionando nova coluna na tabela */
-ALTER TABLE comissoes
-ADD COLUMN data_pagamento timestamp;
-
-ALTER TABLE comissoes
-DROP COLUMN data_pagamento;
-
-/* Inserindo novos dados as tabelas */
-
-INSERT INTO mesas (mesa_codigo,
-                   mesa_situacao,
-                   data_criacao,
-                   data_atualizacao)
-    VALUES ('00010',
-            'A',
-            NOW(),
-            NOW());
-
-INSERT INTO funcionarios (funcionario_codigo,
-                          funcionario_nome,
-                          funcionario_situacao,
-                          funcionario_comissao,
-                          funcionario_cargo,
-                          data_criacao,
-                          data_atualizacao)
-    VALUES ('00007',
-            'Ana Paula Schimitt',
-            'A',
-            5,
-            'QUALIDADE',
-            NOW(),
-            NOW());
-            
-INSERT INTO produtos (produto_codigo,
-                      produto_nome,
-                      produto_valor,
-                      produto_situacao,
-                      data_criacao,
-                      data_atualizacao)
-    VALUES ('00006',
-            'Web Cam',
-            180,
-            'A',
-            NOW(),
-            NOW());
-            
-INSERT INTO vendas (funcionario_id,
-                    mesa_id,
-                    venda_codigo,
-                    venda_valor,
-                    venda_total,
-                    venda_desconto,
-                    venda_situacao,
-                    data_criacao,
-                    data_atualizacao)
-   VALUES (4,
-           4,
-           '00003',
-           2000,
-           2000,
-           0,
-          'A',
-          NOW(),
-          NOW());
-          
-          
-         
-INSERT INTO itens_vendas (produto_id,
-                          vendas_id,
-                          item_valor,
-                          item_quantidade,
-                          item_total,
-                          data_criacao,
-                          data_atualizacao)
-    VALUES (3,
-            3,
-            7,
-            3,
-            21,
-            NOW(),
-            NOW());
-            
-            
-/* Criando FUNCTION para retornar a situação do funcionario */
-CREATE OR REPLACE FUNCTION retorna_nome_funcionario(fund_id int)
-RETURNS text AS
-$$
-DECLARE
-nome text;
-situacao text;
-BEGIN
-    SELECT funcionario_nome,
-           funcionario_situacao
-      INTO nome, situacao
-      FROM funcionarios
-     WHERE id = fund_id;
-     
-     IF situacao = 'A' THEN
-        RETURN nome || ' Usuário Ativo';
-     ELSEIF situacao = 'I' THEN
-        RETURN nome || ' Usuário Inativo';
-     ELSEIF situacao IS NULL THEN
-        RETURN nome || ' Usuário Sem status';
-     ELSE
-        RETURN 'Usuário com status diferente de A e I';
-     END IF;
-END
-$$
-LANGUAGE plpgsql;
-
-DROP FUNCTION retorna_nome_funcionario;
-
-SELECT retorna_nome_funcionario(1);
-
-
-/* FUNCTION para calcular a porcentagem da comissao */
-CREATE OR REPLACE FUNCTION rt_valor_comissao(func_id int)
-RETURNS real AS
-$$
-DECLARE
-valor_comissao real;
-BEGIN
-    SELECT  funcionario_comissao
-       INTO valor_comissao
-       FROM funcionarios
-      WHERE id = func_id;
-      RETURN valor_comissao;
-END
-$$
-LANGUAGE plpgsql;
           
 DROP FUNCTION rt_valor_comissao;
           
 SELECT rt_valor_comissao(1);
 
-/* FUNCTION sem retorno para que faça o calculo das vendas que tenham comissões */
+-- FUNCTION sem retorno para que faça o calculo das vendas que tenham comissões
 CREATE OR REPLACE FUNCTION calc_comissao(data_ini timestamp, data_fim timestamp)
 RETURNS void AS $$
 DECLARE
@@ -797,9 +781,15 @@ BEGIN
 END
 $$
 LANGUAGE plpgsql;
+--FIM--
+
+
 
 
 SELECT calc_comissao('01/01/2016 00:00:00','01/01/2016 00:00:00');
+
+
+
 
 /* Funções para STRING */
 
@@ -919,31 +909,48 @@ SELECT EXTRACT(MONTH FROM TIMESTAMP '23/04/2001 20:34:20');
 -- Para extrair o valor dos segundos de um determinado horário
 SELECT EXTRACT(SECOND FROM TIMESTAMP '23/04/2001 20:34:20');
 
-
 SELECT EXTRACT (YEAR FROM data_criacao)
   FROM funcionarios 
  WHERE id = 1;
 
+
+
+
 -- COUNT para contar quantos registros existem na tabela.
 SELECT COUNT(*)
   FROM funcionarios;
-  
+
+
+
+
 -- SUM para fazer somas na tabela.
 SELECT SUM(venda_total)
   FROM vendas;
-  
+ 
+ 
+ 
+ 
 -- AVG para calcular a média.
 SELECT AVG(produto_valor)
   FROM produtos;
+  
+  
+  
   
 -- MAX para o valor máximo da coluna e MIN para o mínimo.
 SELECT MAX(produto_valor), MIN(produto_valor)
   FROM produtos;
   
+  
+  
+  
 -- Utilizando o GROUP BY
 SELECT produto_id, SUM(item_total)
   FROM itens_vendas
   GROUP BY produto_id;
+  
+  
+  
   
 -- Criando função para apresentar os nomes do produto.
 CREATE OR REPLACE FUNCTION retorna_nome_produto(prod_id int)
@@ -961,10 +968,16 @@ END
 $$
 LANGUAGE plpgsql;
 
+
+
+
 -- Refazendo o SELECT com GROUP BY com a função que retorna o nome dos produtos.
 SELECT retorna_nome_produto(produto_id), SUM(item_total)
   FROM itens_vendas
   GROUP BY produto_id;
+
+
+
 
 -- Ordenação
 SELECT retorna_nome_produto(produto_id) AS Nome, 
